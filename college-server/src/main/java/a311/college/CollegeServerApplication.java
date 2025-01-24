49effcfff7488f6734bf00c6a2d51562b3cadee7
wzb <2482924813@qq.com>
2025-01-24 23:06:57 +0800

    大学点评项目初步架构

---
+++ college-server/src/main/java/a311/college/CollegeServerApplication.java
@@ -0,0 +1,13 @@
+package a311.college;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CollegeServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CollegeServerApplication.class, args);
+    }
+
+}

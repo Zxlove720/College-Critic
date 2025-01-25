354f1f4e11f7bd9c773056733ebf36c5ae41d92a
wzb <2482924813@qq.com>
2025-01-25 10:39:03 +0800

    大学点评：常量类：JWT令牌用户信息常量

---
+++ college-common/src/main/java/a311/college/constant/JWTUserClaimsConstant.java
@@ -0,0 +1,16 @@
+package a311.college.constant;
+
+/**
+ * JWT令牌用户数据Claims
+ */
+public class JWTUserClaimsConstant {
+
+    // 用户id
+    public static final String USERID = "userId";
+
+    // 用户名
+    public static final String USERNAME = "username";
+
+    // 手机号
+    public static final String PHONE_NUMBER = "phone";
+}

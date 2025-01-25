d2c842a4a344d2cc67b43c8c67926c3643de810e
wzb <2482924813@qq.com>
2025-01-25 13:51:38 +0800

    大学点评：通过ThreadLocal操作当前线程局部变量的值

---
+++ college-utils/src/main/java/a311/college/thread/ThreadLocalUtils.java
@@ -0,0 +1,25 @@
+package a311.college.thread;
+
+/**
+ * ThreadLocal工具类
+ */
+public class ThreadLocalUtils {
+
+    public static ThreadLocal<Long> threadLocal = new ThreadLocal<>();
+
+    // 设置当前线程局部变量的值
+    public static void setCurrentId(Long id) {
+        threadLocal.set(id);
+    }
+
+    // 获取当前线程的局部变量的值
+    public static Long getCurrentId() {
+        return threadLocal.get();
+    }
+
+    // 清空当前线程的局部变量的值
+    public static void removeCurrentId() {
+        threadLocal.remove();
+    }
+
+}

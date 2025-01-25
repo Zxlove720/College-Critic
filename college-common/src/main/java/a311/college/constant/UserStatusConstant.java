43bb0b4223f156e7b3828b08f5d39347ecc9d0b6
wzb <2482924813@qq.com>
2025-01-25 10:34:37 +0800

    大学点评：常量类：用户状态常量和公共字段填充相关常量

---
+++ college-common/src/main/java/a311/college/constant/UserStatusConstant.java
@@ -0,0 +1,13 @@
+package a311.college.constant;
+
+/**
+ * 用户状态常量
+ */
+public class UserStatusConstant {
+
+    // 启用
+    public static final Integer ENABLE = 1;
+
+    // 禁用
+    public static final Integer DISABLE = 0;
+}

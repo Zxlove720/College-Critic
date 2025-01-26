b0abfca38f4705dc4db09ea5391fec4a025da026
wzb <2482924813@qq.com>
2025-01-26 14:42:19 +0800

    大学点评：用户分页查询DTO

---
+++ college-pojo/src/main/java/a311/college/dao/UserPageQueryDTO.java
@@ -0,0 +1,13 @@
+package a311.college.dao;
+
+public class UserPageQueryDTO {
+
+    // 用户名
+    private String username;
+
+    // 页码
+    private Integer page;
+
+    // 每页记录数
+    private Integer pageSize;
+}

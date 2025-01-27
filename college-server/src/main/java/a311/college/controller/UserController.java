45f5b9f42acee3ca8466d9c0504c31f9a016f693
wzb <2482924813@qq.com>
2025-01-27 21:30:42 +0800

    大学点评：UserService：用户退出

--- college-server/src/main/java/a311/college/controller/UserController.java
+++ college-server/src/main/java/a311/college/controller/UserController.java
@@ -5,11 +5,14 @@ import a311.college.dao.UserLoginDTO;
 import a311.college.entity.User;
 import a311.college.jwt.JWTUtils;
 import a311.college.properties.JWTProperties;
+import a311.college.result.PageResult;
 import a311.college.result.Result;
 import a311.college.service.UserService;
 import a311.college.vo.UserLoginVO;
+import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -38,6 +41,8 @@ public class UserController {
      * @param userLoginDTO 封装用户登录数据的DTO
      * @return 用户登录结果VO
      */
+    @PostMapping("/login")
+    @ApiOperation(value = "员工登录")
     public Result<UserLoginVO> login(@RequestBody UserLoginDTO userLoginDTO) {
         log.info("用户{}正在登录", userLoginDTO.getUsername());
 
@@ -60,5 +65,18 @@ public class UserController {
 
         return Result.success(userLoginVO);
     }
+
+    /**
+     * 员工推出
+     * @return Result统一返回结果
+     */
+    @PostMapping("/layout")
+    @ApiOperation(value = "用户退出")
+    public Result<Void> layout() {
+        log.info("用户退出登录");
+        return Result.success();
+    }
+
+
 
 }

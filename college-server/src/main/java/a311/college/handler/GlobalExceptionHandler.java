5322a74b274f3af88aceb2311b9614f2b7eb2b8d
wzb <2482924813@qq.com>
2025-02-02 15:13:34 +0800

    大学点评：UserService：全局异常处理器

---
+++ college-server/src/main/java/a311/college/handler/GlobalExceptionHandler.java
@@ -0,0 +1,52 @@
+package a311.college.handler;
+
+
+import a311.college.constant.MessageConstant;
+import a311.college.exception.BaseException;
+import a311.college.result.Result;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.sql.SQLIntegrityConstraintViolationException;
+
+@Slf4j
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    /**
+     * 捕获业务异常
+     * @param exception 业务异常
+     * @return Result<Void>
+     */
+    @ExceptionHandler
+    public Result<Void> exceptionHandler(BaseException exception) {
+        log.info("异常信息：{}", exception.getMessage());
+        return Result.error(exception.getMessage());
+    }
+
+    /**
+     * 处理SQL异常
+     * @param exception SQL异常
+     * @return Result<Void>
+     */
+    public Result<Void> exceptionHandler(SQLIntegrityConstraintViolationException exception) {
+        // 获得报错信息
+        String message = exception.getMessage();
+        // 解决重复添加问题
+        if (message.contains("Duplicate entry")) {
+            // 按照空格对报错信息进行切片处理，获取到重复添加的值
+            String[] split = message.split(" ");
+            // 获取重复添加的用户名
+            String username = split[2];
+            // 拼接报错信息
+            String errorMessage = username + MessageConstant.ALREADY_EXISTS;
+            return Result.error(errorMessage);
+        } else {
+            // 并非重复添加报错
+            // TODO日后可以根据需求添加更多的SQL异常处理
+            return Result.error(MessageConstant.UNKNOWN_ERROR);
+        }
+    }
+
+}

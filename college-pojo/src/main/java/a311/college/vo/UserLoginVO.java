0c751e1ab346e0010dcdcaeaf176b272282c2a85
wzb <2482924813@qq.com>
2025-02-26 00:10:58 +0800

    大学点评：Knife4j配置

--- college-pojo/src/main/java/a311/college/vo/UserLoginVO.java
+++ college-pojo/src/main/java/a311/college/vo/UserLoginVO.java
@@ -1,5 +1,6 @@
 package a311.college.vo;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -16,10 +17,13 @@ import java.io.Serializable;
 @AllArgsConstructor
 public class UserLoginVO implements Serializable {
 
+    @Schema(description = "用户id")
     private Long id;
 
+    @Schema(description = "用户名")
     private String username;
 
+    @Schema(description = "JWT令牌")
     private String token;
 
 }

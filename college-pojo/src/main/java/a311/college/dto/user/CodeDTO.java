7ae96fcd4f8b2cfbc2d8aeb0786bc1201cab15ad
wzb <2482924813@qq.com>
2025-03-14 00:00:53 +0800

    跃鲤志选：用户服务完善

--- college-pojo/src/main/java/a311/college/dto/user/CodeDTO.java
+++ college-pojo/src/main/java/a311/college/dto/user/CodeDTO.java
@@ -1,9 +1,12 @@
 package a311.college.dto.user;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Data;
 
 @Data
+@Schema(description = "验证码DTO")
 public class CodeDTO {
 
+    @Schema(description = "手机号")
     private String phone;
 }
--- college-pojo/src/main/java/a311/college/dto/user/DeleteDTO.java
+++ college-pojo/src/main/java/a311/college/dto/user/DeleteDTO.java
@@ -4,6 +4,7 @@ import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Data;
 
 @Data
+@Schema(description = "用户注销DTO")
 public class DeleteDTO {
 
     @Schema(description = "手机号")

7cd349d4ac835c2a8f9ff8d626f778ec496e9545
wzb <2482924813@qq.com>
2025-03-17 20:29:15 +0800

    跃鲤志选：大学优化

--- college-server/src/main/java/a311/college/service/impl/CollegeServiceImpl.java
+++ college-server/src/main/java/a311/college/service/impl/CollegeServiceImpl.java
@@ -149,11 +149,10 @@ public class CollegeServiceImpl implements CollegeService {
 
     /**
      * 优化大学数据
-     *
      */
     @Override
     public void addScore() {
-        List<CollegeSimpleVO>list = collegeMapper.getAllCollege();
+        List<CollegeSimpleVO> list = collegeMapper.getAllCollege();
         for (CollegeSimpleVO collegeSimpleVO : list) {
             String rankList = collegeSimpleVO.getRankList();
             String[] split = rankList.split(",");
@@ -165,32 +164,95 @@ public class CollegeServiceImpl implements CollegeService {
 
     @Override
     public CollegeVO getCollege(CollegeDTO collegeDTO) {
+        // 1.获取简略大学信息
         CollegeSimpleVO collegeSimpleVO = collegeMapper.selectBySchoolId(collegeDTO.getCollegeId());
+        // 2.封装大学详细信息
         CollegeVO collegeVO = new CollegeVO();
         BeanUtil.copyProperties(collegeSimpleVO, collegeVO);
+        // 3.返回随机校园风光
+        // 3.1获取所有照片
         List<String> imageList = resourceMapper.getAllImages();
+        // 3.2从所有照片中随机选取6张
         List<String> images = new ArrayList<>();
         for (int i = 0; i < 6; i++) {
             int index = RandomUtil.randomInt(0, 250);
             images.add(imageList.get(index));
         }
+        // 3.3返回随机6张校园风光
         collegeVO.setImages(images);
+        // 4.随机校园配置
+        if (collegeSimpleVO.getScore() > 60) {
+            // 4.1该学校属于好学校
+            collegeVO.setEquipment(highScoreSchool());
+        } else if (collegeSimpleVO.getRankList().contains("民办")) {
+            // 4.2该学校属于有钱的学校
+            collegeVO.setEquipment(richSchool());
+        } else {
+            // 4.3该学校属于一般学校
+            collegeVO.setEquipment(commonSchool());
+        }
+        return collegeVO;
+    }
 
 
-        return null;
+    private Map<String, Integer> highScoreSchool() {
+        Map<String, Integer> equipment = new HashMap<>();
+        equipment.put("one", 4);
+        equipment.put("two", 1);
+        equipment.put("three", RandomUtil.randomInt(5, 16));
+        equipment.put("four", 1);
+        equipment.put("five", 1);
+        equipment.put("six", 1);
+        equipment.put("seven", 1);
+        equipment.put("eight", 1);
+        equipment.put("nine", 1);
+        equipment.put("ten", RandomUtil.randomInt(5, 11));
+        return equipment;
+    }
+
+    private Map<String, Integer> richSchool() {
+        Map<String, Integer> equipment = new HashMap<>();
+        equipment.put("one", 4);
+        equipment.put("two", 1);
+        equipment.put("three", RandomUtil.randomInt(3, 7));
+        equipment.put("four", 1);
+        equipment.put("five", 1);
+        equipment.put("six", 1);
+        equipment.put("seven", 1);
+        equipment.put("eight", 1);
+        equipment.put("nine", 1);
+        equipment.put("ten", RandomUtil.randomInt(2, 5));
+        return equipment;
+    }
+
+    private Map<String, Integer> commonSchool() {
+        Map<String, Integer> equipment = new HashMap<>();
+        Integer[] room = {4, 6};
+        equipment.put("one", RandomUtil.randomEle(room));
+        Integer[] flag = {0, 1};
+        equipment.put("two", RandomUtil.randomEle(flag));
+        equipment.put("three", RandomUtil.randomInt(5, 11));
+        equipment.put("four", 1);
+        equipment.put("five", 1);
+        equipment.put("six", RandomUtil.randomEle(flag));
+        equipment.put("seven", 1);
+        equipment.put("eight", 0);
+        equipment.put("nine", RandomUtil.randomEle(flag));
+        equipment.put("ten", RandomUtil.randomInt(3, 7));
+        return equipment;
     }
 
     private int getScore(CollegeSimpleVO collegeSimpleVO, String[] split) {
         int score = 0;
         for (String s : split) {
-             switch (s) {
-                 case "本科", "双一流", "强基计划" -> score += 15;
-                 case "公办", "军事类" -> score += 10;
-                 case "985" -> score += 30;
-                 case "211" -> score += 20;
-                 case "医药类" -> score += 5;
-                 case "双高计划" -> score += 3;
-             }
+            switch (s) {
+                case "本科", "双一流", "强基计划" -> score += 15;
+                case "公办", "军事类" -> score += 10;
+                case "985" -> score += 30;
+                case "211" -> score += 20;
+                case "医药类" -> score += 5;
+                case "双高计划" -> score += 3;
+            }
         }
         if (collegeSimpleVO.getSchoolName().contains("大学")) {
             score += 5;
@@ -199,7 +261,7 @@ public class CollegeServiceImpl implements CollegeService {
     }
 
     private void updateRankList() {
-        List<CollegeSimpleVO>list = collegeMapper.getAllCollege();
+        List<CollegeSimpleVO> list = collegeMapper.getAllCollege();
         for (CollegeSimpleVO collegeSimpleVO : list) {
             String rankList = collegeSimpleVO.getRankList();
             String tempResult = rankList.replace("[", "");

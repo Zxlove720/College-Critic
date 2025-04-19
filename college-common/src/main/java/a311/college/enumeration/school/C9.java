5207e0888b6afc92d33dc15d1ac4ae15990198b0
wzb <2482924813@qq.com>
2025-04-19 22:17:07 +0800

    跃鲤志选：服务优化

---
+++ college-common/src/main/java/a311/college/enumeration/school/C9.java
@@ -0,0 +1,13 @@
+package a311.college.enumeration.school;
+
+public enum C9 {
+    清华大学,
+    北京大学,
+    浙江大学,
+    上海交通大学,
+    复旦大学,
+    南京大学,
+    中国科学技术大学,
+    西安交通大学,
+    哈尔滨工业大学
+}
---
+++ college-common/src/main/java/a311/college/enumeration/school/Defense7.java
@@ -0,0 +1,11 @@
+package a311.college.enumeration.school;
+
+public enum Defense7 {
+    哈尔滨工业大学,
+    哈尔滨工程大学,
+    北京航空航天大学,
+    北京理工大学,
+    西北工业大学,
+    南京航空航天大学,
+    南京理工大学
+}

5dc960cfcfcfc58467563d4cd941caa5bd0f0607
wzb <2482924813@qq.com>
2025-05-06 22:41:16 +0800

    跃鲤志选：服务优化

--- college-server/src/main/java/a311/college/mapper/volunteer/VolunteerMapper.java
+++ college-server/src/main/java/a311/college/mapper/volunteer/VolunteerMapper.java
@@ -20,6 +20,15 @@ public interface VolunteerMapper {
     List<SchoolVolunteer> selectVolunteerSchool(VolunteerPageDTO volunteerPageDTO);
 
     /**
+     * 判断志愿表是否重复创建
+     *
+     * @param tableName 志愿表名
+     * @param userId    用户id
+     */
+    @Select("select count(table_id) from tb_volunteer_table where table_name = #{tableName} and user_id = #{userId}")
+    Integer checkVolunteerTable(String tableName, long userId);
+
+    /**
      * 根据id查询专业
      *
      * @param majorId 专业id
@@ -34,19 +43,6 @@ public interface VolunteerMapper {
      */
     @Select("select count from tb_volunteer where table_id = #{tableId} order by count desc limit 1")
     Integer getCount(int tableId);
-
-    /**
-     * 添加志愿
-     *
-     * @param volunteer 志愿实体
-     */
-    @Insert("insert into tb_volunteer(user_id, table_id, category, school_id, school_head, school_name, " +
-            "school_province, rank_list, major_id, major_name, first_choice, other_choice, " +
-            "special, count, year, min_score, min_ranking, scoreThanMe, rankingThanMe) values " +
-            "(#{userId}, #{tableId}, #{category}, #{schoolId}, #{schoolHead}, #{schoolName}, #{schoolProvince}, " +
-            "#{rankList}, #{majorId}, #{majorName}, #{firstChoice}, #{otherChoice}, #{special}, #{count}, " +
-            "#{year}, #{minScore}, #{minRanking}, #{scoreThanMe}, #{rankingThanMe})")
-    void addVolunteer(Volunteer volunteer);
 
     /**
      * 判断志愿是否被用户收藏
@@ -100,13 +96,17 @@ public interface VolunteerMapper {
     List<VolunteerTable> selectTables(Long userId);
 
     /**
-     * 查询志愿表内容
+     * 添加志愿
      *
-     * @param tableId 志愿表id
-     * @return List<Volunteer>
+     * @param volunteer 志愿实体
      */
-    @Select("select * from tb_volunteer where table_id = #{tableId} order by count")
-    List<Volunteer> selectVolunteers(Integer tableId);
+    @Insert("insert into tb_volunteer(user_id, table_id, category, school_id, school_head, school_name, " +
+            "school_province, rank_list, major_id, major_name, first_choice, other_choice, " +
+            "special, count, year, min_score, min_ranking, scoreThanMe, rankingThanMe) values " +
+            "(#{userId}, #{tableId}, #{category}, #{schoolId}, #{schoolHead}, #{schoolName}, #{schoolProvince}, " +
+            "#{rankList}, #{majorId}, #{majorName}, #{firstChoice}, #{otherChoice}, #{special}, #{count}, " +
+            "#{year}, #{minScore}, #{minRanking}, #{scoreThanMe}, #{rankingThanMe})")
+    void addVolunteer(Volunteer volunteer);
 
     /**
      * 删除志愿
@@ -117,12 +117,12 @@ public interface VolunteerMapper {
     void deleteVolunteer(Volunteer volunteer);
 
     /**
-     * 判断志愿表是否重复创建
+     * 查询志愿表内容
      *
-     * @param tableName 志愿表名
-     * @param userId    用户id
+     * @param tableId 志愿表id
+     * @return List<Volunteer>
      */
-    @Select("select count(table_id) from tb_volunteer_table where table_name = #{tableName} and user_id = #{userId}")
-    Integer checkVolunteerTable(String tableName, long userId);
+    @Select("select * from tb_volunteer where table_id = #{tableId} order by count")
+    List<Volunteer> selectVolunteers(Integer tableId);
 
 }
